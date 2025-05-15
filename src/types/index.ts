e9dcd6374662b2d3ca0ddb5fc22d0b8904e18a22
mhmitas <mahfuzulmitas@gmail.com>
2025-05-15 21:47:34 +0600

    make searchParams required in SearchParamProps type

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,5 +19,5 @@ export type RemoveUrlQueryParams = {
 
 export type SearchParamProps = {
     params?: { id: string }
-    searchParams?: { [key: string]: string | string[] | undefined }
+    searchParams: { [key: string]: string | string[] | undefined }
 }
